73142e338873ed1ee87f86258dbb28b5d96f65ea
Sam Walker <sgwalker327@gmail.com>
2023-06-01 15:19:40 -0600

    Generated Tea, Subscription, Subscription_tea, and Customer models

---
+++ spec/factories/customers.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :customer do
+    first_name { "MyString" }
+    last_name { "MyString" }
+    email { "MyString" }
+    address { "MyString" }
+    subscription { nil }
+  end
+end
