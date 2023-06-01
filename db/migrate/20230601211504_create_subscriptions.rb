11f870a2785e0193731150ff5e1f75ff0c3bd52e
Sam Walker <sgwalker327@gmail.com>
2023-06-01 15:50:42 -0600

    have been sorting out postgresql connection error for 20 mins

--- db/migrate/20230601211504_create_subscriptions.rb
+++ db/migrate/20230601211504_create_subscriptions.rb
@@ -2,7 +2,7 @@ class CreateSubscriptions < ActiveRecord::Migration[7.0]
   def change
     create_table :subscriptions do |t|
       t.string :title
-      t.integer :price
+      t.float :price
       t.integer :status
       t.integer :frequency
 
