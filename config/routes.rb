f15394d8ccf9a35b40cf89ffb1bffb023db13547
Sam Walker <sgwalker327@gmail.com>
2023-06-04 15:48:55 -0600

    Updated routes with proper namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :customers do
-    resources :subcriptions, only: [:index, :update, :destroy]
+  resources :api do
+    resources :v1 do
+      resources :customers do
+        resources :subcriptions, only: [:index, :update, :destroy]
+      end
+    end
   end
 end
