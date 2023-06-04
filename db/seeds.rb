02e5f07f07414fc2a1584b4dcd8ddfd134c5b57e
Sam Walker <sgwalker327@gmail.com>
2023-06-04 15:03:01 -0600

    Added initial seed data and model relationship tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+c1 = Customer.create!(first_name: "Steve", last_name: "Steverson", email: "steve@steve.com", address: "123 Main St, Minturn, CO 81645")
+c2 = Customer.create!(first_name: "Ruth", last_name: "Rutherson", email: "ruth@ruth.com", address: "123 Main St, Avon, CO 81620")
+
+green = Tea.create!(title: "Green Tea", description: "Green tea is a tea characterized by it's green color.", temperature: 180, brew_time: 180)
+earl = Tea.create!(title: "Earl Grey", description: "Earl Grey is tea made famous by the Earl of Grey.", temperature: 160, brew_time: 210)
+black = Tea.create!(title: "Black Tea", description: "Black tea is black.", temperature: 170, brew_time: 190)
+
+sub1 = Subscription.create!(title: "Green Tea Everyday", price: 10.00, status: 0, frequency: 0, tea_id: green.id)
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to :tea }
+    it { should have_many :customer_subscriptions }
+    it { should have_many(:customers).through(:customer_subscriptions) }
+  end
 end
