6716b39134410e912e6e13e68c181f12517e27da
Sam Walker <sgwalker327@gmail.com>
2023-06-04 15:22:16 -0600

    Added frequency enum to subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,4 +2,6 @@ class Subscription < ApplicationRecord
   belongs_to :tea
   has_many :customer_subscriptions
   has_many :customers, through: :customer_subscriptions
+
+  enum frequency: [:monthly, :biweekly, :weekly]
 end
